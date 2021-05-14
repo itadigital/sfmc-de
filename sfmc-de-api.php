87b5831fa1854d9494b6c27e5fc87fb25219ed70
Chris Saldanha <csaldanha@itagroup.com>
2021-05-14 16:28:54 -0500

    no message

--- sfmc-de-api.php
+++ sfmc-de-api.php
@@ -14,7 +14,7 @@ $ch = curl_init($url);
 
 //Encode the array into JSON.
 $jsonDataEncoded = json_encode($jsonData);
-
+/*
 //Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
 
@@ -24,7 +24,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'); 
 
-/*
+
 //Execute the request
 $result = curl_exec($ch);
 if(curl_errno($ch)){
