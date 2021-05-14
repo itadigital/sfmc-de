efa526977ad97fd055f3c3fcd3892da124734681
Chris Saldanha <csaldanha@itagroup.com>
2021-05-14 16:11:31 -0500

    no message

--- sfmc-de-api.php
+++ sfmc-de-api.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$url = 'https://mc-sc7jp15w47twh8j85754875m4.auth.marketingcloudapis.com//v2/token';
+$url = 'https://mc-sc7jp15w47twh8j85754875m4.auth.marketingcloudapis.com/v2/token';
 $data = array('grant_type' => 'client_credentials', 'client_id' => '3z8wse7eiggnaggips7uihso', 'client_secret' => 'nD0lGBUZWg5K7Oee4RZS5VEe');
 
 // use key 'http' even if you send the request to https://...
