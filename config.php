b1613e64627b636b29ef53282aa4913c566b5b3e
Chris Saldanha <csaldanha@itagroup.com>
2021-05-14 11:56:45 -0500

    Install sdk files

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'appsignature' => 'none',
+	'clientid' => '3z8wse7eiggnaggips7uihso',
+	'clientsecret' => 'nD0lGBUZWg5K7Oee4RZS5VEe',
+	'defaultwsdl' => 'https://mc-sc7jp15w47twh8j85754875m4.soap.marketingcloudapis.com/etframework.wsdl',
+    'baseUrl' => 'https://mc-sc7jp15w47twh8j85754875m4.rest.marketingcloudapis.com/',
+    'baseAuthUrl' => 'https://mc-sc7jp15w47twh8j85754875m4.auth.marketingcloudapis.com/',
+'baseSoapUrl' => 'https://mc-sc7jp15w47twh8j85754875m4.soap.marketingcloudapis.com/',
+);
---
+++ init.php
@@ -0,0 +1,6 @@
+require('sdk/ET_Client.php');
+$client = new ET_Client();
+$request = new ET_List();
+$request->authStub = $client;
+$response = $request->get();
+print_r($response);
